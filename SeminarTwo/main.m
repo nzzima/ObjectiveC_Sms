50b6435313471dc303883a9ccf00c3faac2416ad
nzzima <nikita-krylov-2012@mail.ru>
2024-11-12 10:52:36 +0300

    Start SeminarTwo

---
+++ SeminarTwo/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  Seminars
+//
+//  Created by Nikita Krylov on 12.11.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+    }
+    return 0;
+}
