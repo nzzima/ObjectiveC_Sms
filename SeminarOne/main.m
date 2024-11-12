b1875c50f289cfa0c11006540b35837d031e0791
nzzima <nikita-krylov-2012@mail.ru>
2024-11-12 10:50:02 +0300

    Initial Commit

---
+++ SeminarOne/main.m
@@ -0,0 +1,34 @@
+//
+//  main.m
+//  SeminarOne
+//
+//  Created by Nikita Krylov on 11.11.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        char input[100];
+        NSLog(@"Enter string: ");
+        scanf("%s", input);
+        NSString *inputStr = [NSString stringWithCString:input encoding:NSUTF8StringEncoding];
+//        for (int i = 0; i < inputStr.length; i++) {
+//            //NSLog(@" %c", [inputStr characterAtIndex:i]);
+//            printf("%c", [inputStr characterAtIndex:i]);
+//        }
+        int i = 0;
+        
+//        while (i < inputStr.length) {
+//            printf(" %c", [inputStr characterAtIndex:i]);
+//            i++;
+//        }
+        
+        do {
+            printf(" %c", [inputStr characterAtIndex:i]);
+            i++;
+        } while (i < inputStr.length);
+        printf("\n");
+    }
+    return 0;
+}
