5dcbcaf111e290194fac9861507e9e430ac5807b
nzzima <nikita-krylov-2012@mail.ru>
2024-11-12 14:45:24 +0300

    Decommit sem1

--- SeminarOne/main.m
+++ SeminarOne/main.m
@@ -7,28 +7,28 @@
 
 #import <Foundation/Foundation.h>
 
-//int main(int argc, const char * argv[]) {
-//    @autoreleasepool {
-//        char input[100];
-//        NSLog(@"Enter string: ");
-//        scanf("%s", input);
-//        NSString *inputStr = [NSString stringWithCString:input encoding:NSUTF8StringEncoding];
-////        for (int i = 0; i < inputStr.length; i++) {
-////            //NSLog(@" %c", [inputStr characterAtIndex:i]);
-////            printf("%c", [inputStr characterAtIndex:i]);
-////        }
-//        int i = 0;
-//        
-////        while (i < inputStr.length) {
-////            printf(" %c", [inputStr characterAtIndex:i]);
-////            i++;
-////        }
-//        
-//        do {
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        char input[100];
+        NSLog(@"Enter string: ");
+        scanf("%s", input);
+        NSString *inputStr = [NSString stringWithCString:input encoding:NSUTF8StringEncoding];
+//        for (int i = 0; i < inputStr.length; i++) {
+//            //NSLog(@" %c", [inputStr characterAtIndex:i]);
+//            printf("%c", [inputStr characterAtIndex:i]);
+//        }
+        int i = 0;
+        
+//        while (i < inputStr.length) {
 //            printf(" %c", [inputStr characterAtIndex:i]);
 //            i++;
-//        } while (i < inputStr.length);
-//        printf("\n");
-//    }
-//    return 0;
-//}
+//        }
+        
+        do {
+            printf(" %c", [inputStr characterAtIndex:i]);
+            i++;
+        } while (i < inputStr.length);
+        printf("\n");
+    }
+    return 0;
+}
